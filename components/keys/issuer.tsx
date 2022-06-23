3c27da728a383e90273243b00414320c935d455a
Matt Venables <matt@m2.xyz>
2022-06-23 17:15:37 -0400

    Show verifier on the keys page

--- components/keys/issuer.tsx
+++ components/keys/issuer.tsx
@@ -8,11 +8,12 @@ import toast from "react-hot-toast"
 import {
   formatDidKey,
   formatSecret,
-  CredentialIssuer
+  CredentialIssuer,
+  CredentialVerifier
 } from "lib/credential-fns"
 
 type Props = {
-  issuer: CredentialIssuer
+  issuer: CredentialIssuer | CredentialVerifier
 }
 
 const Issuer: FC<Props> = ({ issuer }) => {
@@ -32,16 +33,18 @@ const Issuer: FC<Props> = ({ issuer }) => {
       <li key={issuer.name} className="flex py-4 space-x-8">
         <div className="w-1/4">
           <p className="text-lg font-medium text-gray-900">{issuer.name}</p>
-          <span
-            className={clsx(
-              "inline-flex items-center px-2 py-0.5 rounded text-xs font-medium",
-              issuer.isTrusted
-                ? "bg-green-100 text-green-800"
-                : "bg-gray-100 text-gray-800"
-            )}
-          >
-            {issuer.isTrusted ? "Trusted" : "Untrusted"}
-          </span>
+          {"isTrusted" in issuer && (
+            <span
+              className={clsx(
+                "inline-flex items-center px-2 py-0.5 rounded text-xs font-medium",
+                issuer.isTrusted
+                  ? "bg-green-100 text-green-800"
+                  : "bg-gray-100 text-gray-800"
+              )}
+            >
+              {issuer.isTrusted ? "Trusted" : "Untrusted"}
+            </span>
+          )}
         </div>
         <div className="flex flex-col space-y-2 overflow-hidden">
           <div>
